062e384c295db091adc2c76e449b92de9a87cf7f
mxxxxxs <relife_abyss@qq.com>
2018-12-05 22:36:59 +0800

    初步完成输入信号散点图的响应式更新

--- signal.js
+++ signal.js
@@ -1,70 +1,57 @@
 class Complex {
-    constructor(r, i) {
-        this.r = r
-        this.i = i
-    }
-    mult(complex) {
-        let newR = this.r * complex.r - this.i * complex.i,
-            newI = this.i * complex.r + this.r * complex.i
-        return new Complex(newR, newI)
-    }
-    div(complex) {
-        if (complex.r ** 2 + complex.i ** 2 != 0) {
-            let newR = (this.r * complex.r + this.i * complex.i) / (complex.r ** 2 + complex.i ** 2),
-                newI = -(this.r * complex.i - this.i * complex.r) / (complex.r ** 2 + complex.i ** 2)
-            return new Complex(newR, newI)
-        }
-    }
-    add(complex) {
-        let newR = this.r + complex.r,
-            newI = this.i + complex.i
-        return new Complex(newR, newI)
-    }
-    fix(n = 4) {
-        function fixPrecision(num, n) {
-            let buf = num * 10 ** n
-            buf = Math.round(buf)
-            buf /= 10 ** n
-            return buf
-        }
-        return new Complex(fixPrecision(this.r, n), fixPrecision(this.i, n))
-    }
-
-}
-let sigArr = [
-    [3.5, 23],
-    [2.9, 6],
-    [26, 49],
-    [5.6, 77],
-    [35,44],
-    [4.3, 6.3],
-    [34,26.3],
-    [36.3, 22],
-    [32, 23],
-    [3.3, 65],
-    [3.5, 4.3],
-    [5.3, 15],
-    [31.4, 24.6],
-    [14, 3],
-    [3.2, 13],
-    [36,48]
-]
-
-let signals = sigArr.map(sig => new Complex(sig[0], sig[1]))
-function genSig(sig, n) {
-  function longer(n) {
-    if (n > 0) {
-      sig = sig.concat(sig)
-      n--
-      longer(n)
-    }
-    return sig
+  constructor(r, i) {
+    this.r = r
+    this.i = i
   }
-  return longer(n)
+  mult(complex) {
+    let newR = this.r * complex.r - this.i * complex.i,
+      newI = this.i * complex.r + this.r * complex.i
+    return new Complex(newR, newI)
+  }
+  div(complex) {
+    if (complex.r ** 2 + complex.i ** 2 != 0) {
+      let newR = (this.r * complex.r + this.i * complex.i) / (complex.r ** 2 + complex.i ** 2),
+        newI = -(this.r * complex.i - this.i * complex.r) / (complex.r ** 2 + complex.i ** 2)
+      return new Complex(newR, newI)
+    }
+  }
+  add(complex) {
+    let newR = this.r + complex.r,
+      newI = this.i + complex.i
+    return new Complex(newR, newI)
+  }
+  fix(n = 4) {
+    function fixPrecision(num, n) {
+      let buf = num * 10 ** n
+      buf = Math.round(buf)
+      buf /= 10 ** n
+      return buf
+    }
+    return new Complex(fixPrecision(this.r, n), fixPrecision(this.i, n))
+  }
 }
+
+function sigArr(n, xScale, yScale) {
+  let sig = []
+  while (sig.length < n) {
+    sig.push([Math.random() * (xScale[1] - xScale[0]) + xScale[0],
+    Math.random() * (yScale[1] - yScale[0]) + yScale[0]
+    ])
+  }
+  return sig
+}
+
+function sig(nOrArr, xScale, yScale) {
+  return arguments.length == 1 && Array.isArray(arguments[0])
+  ? nOrArr.map(sig => new Complex(sig[0], sig[1]))
+  : arguments.length == 3 
+  ? sigArr(nOrArr, xScale, yScale).map(sig => new Complex(sig[0], sig[1]))
+  : undefined
+}
+
 // let signals = genSig(basicSignals, 0)
 module.exports = {
-    sigArr,
-    signals,
-    Complex
+  sigArr,
+  sig,
+  Complex
 }
--- visualize.js
+++ visualize.js
@@ -1,76 +1,300 @@
-let d3 = Object.assign({}, require('d3-selection'), require('d3-scale'), require('d3-array'), require('d3-axis'))
+let d3 = require('d3')
 let Signal = require('./fft.js')
 let {
   sigArr,
-  signals
+  sig
 } = require('./signal.js')
 
-let inSig = signals
-let outSig = new Signal(inSig)
+let inSig
+let outSig
+let FFTed
+let amplitude
+let phase
+let real
+let imaginary
 
-let FFTed = outSig.FFT().output //FFT变换后的信号
+//图表样式
+let chartStyle = {
+  w: 700,
+  h: 700,
+  padding: 80,
+  pointSize: 5,
 
-let amplitude = FFTed.map(val => Math.sqrt(val.r ** 2 + val.i ** 2))
-let phase = FFTed.map(val => Math.atan(val.i / val.r))
-let real = FFTed.map(val => val.r)
-let imagine = FFTed.map(val => val.i)
+}
+//工具函数
+let ezBindAll = (obj, fn, _this) => {
+  return new Proxy(obj, {
+    set: function (target, key, val) {
+      let oldVal = target[key]
+      let newVal = val
+      target[key] = val
+      fn(newVal, oldVal, key, _this)
+      return true
+    }
+  })
+}
 
-console.log(`${amplitude}\n${phase}\n${real}\n${imagine}`);
+let once = (function () {
+  let n = 1
+  function count(fn) {
+    if (n === 1) {
+      n = 0
+      return fn
+    }
+    return () => {}
+  }
+  return count
+})()
 
-let w = 500 //x轴长
-let h = 400 //y轴长
-let padding = 50  //让边缘的点显示完全
-let pointSize = 4
 
-let xScale = d3.scaleLinear()
-  .domain([d3.min(sigArr, d => d[0]), d3.max(sigArr, d => d[0])])
-  .range([padding, w - padding])
-let yScale = d3.scaleLinear()
-  .domain([d3.min(sigArr, d => d[1]), d3.max(sigArr, d => d[1])])
-  .range([h - padding, padding])
 
-let chart = d3.select('.input-signal')
-  .append('svg')
-  .attr('width', w)
-  .attr('height', h)
+class InSigScatter {
+  constructor(chartStyle) {
+    let _this = this
+    //初始化图表参数
+    this.w = chartStyle.w
+    this.h = chartStyle.h
+    this.padding = chartStyle.padding
+    this.pointSize = chartStyle.pointSize
+    //监听事件绑定input输入
+    this.userInput = ezBindAll({}, this.whenInputed, this)
+    this.inputs = d3.selectAll('input')
+    this.inputs.on('change', function () {
+      _this.userInput[this.id] = parseFloat(this.value)
+    })
+    //创建svg
+    this.inSigScatter = d3.select('.input-signal')
+    this.inSigScatter
+      .append('svg')
+      .attr('width', this.w)
+      .attr('height', this.h)
+    this.svg = this.inSigScatter.select('svg')
+  }
 
-chart.selectAll('circle')
-  .data(sigArr)
-  .enter()
-  .append('circle')
-  .attr('cx', d => xScale(d[0]))
-  .attr('cy', d => yScale(d[1]))
-  .attr('r', pointSize)
-  .attr("fill", "rgb(149, 180, 54)");
+  whenInputed(newVal, oldVal, id, _this) {
+    if (newVal != oldVal) {
+      //_this.userInput的key对应input的id, 用来绑定
+      document.querySelector(`#${id}`).value = newVal
+      _this.updateChart(_this, id, newVal)
+    }
+  }
 
-// chart.selectAll("text")
-//   .data(sigArr)
-//   .enter()
-//   .append("text")
-//   .text(function (d) {
-//     return d[0] + "," + d[1];
-//   })
-//   .attr('transform', `translate(${pointSize + 2}, ${pointSize / 2})`)
-//   .attr("x", function (d) {
-//     return xScale(d[0]);
-//   })
-//   .attr("y", function (d) {
-//     return yScale(d[1]);
-//   })
-//   .attr("font-family", "sans-serif")
-//   .attr("font-size", "14px")
-//   .attr("fill", "rgb(149, 180, 54)");
+  updateChart(_this, id, newVal) {
+    //收集input的参数, 收集满时开始绘制
+    let dataReady = true
+    _this.inputs.nodes().forEach(node => {
+      if (node.value === '') {
+        dataReady = false
+      } else {
+        _this[id] = newVal
+      }
+    })
+    //input参数收集完成
+    if (dataReady) {
+      _this.inputSig = sigArr(_this.num, [_this.rMin, _this.rMax], [_this.iMin, _this.iMax])
+      console.log(_this.inputSig)
+      //初始绘制, 单此执行
+      once(_this.genInSigScatter)(_this)
+      //变换数据点
+      _this.updateInSigScatter(_this)
+    }
+  }
 
-let xAxis = d3.axisBottom()
-xAxis.scale(xScale)
-chart.append('g')
-  .attr('class', 'axis')
-  .attr('transform', 'translate(0, ' + (h - padding / 2) + ')')
-  .call(xAxis)
-  
-  let yAxis = d3.axisLeft()
-  yAxis.scale(yScale)
-  chart.append('g')
-  .attr('class', 'axis')
-  .attr('transform', `translate(${padding / 2}, 0)`)
-  .call(yAxis)
+  updateInSigScatter(_this) {
+    let svg = _this.svg
+    let w = _this.w
+    let h = _this.h
+    //让边缘的点和坐标轴之间保留空隙
+    let padding = _this.padding
+    let pointSize = _this.pointSize
+    _this.xScale
+      .domain([d3.min(_this.inputSig, d => d[0]), d3.max(_this.inputSig, d => d[0])])
+    _this.yScale
+      .domain([d3.min(_this.inputSig, d => d[1]), d3.max(_this.inputSig, d => d[1])])
+
+    let circles = svg.selectAll('circle')
+      .data(_this.inputSig)
+    circles.enter()
+      .append('circle')
+      .transition()
+      .duration(1000)
+      .attr('cx', d => _this.xScale(d[0]))
+      .attr('cy', d => _this.yScale(d[1]))
+      .attr('r', pointSize)
+      .attr("fill", "rgb(149, 180, 54)")
+
+    circles.exit()
+      .transition()
+      .duration(1000)
+      .attr('cx', d => _this.xScale(d[0]) + w)
+      .attr('cy', d => _this.yScale(d[1]) + h)
+      .remove()
+
+    d3.select('.x')
+      .transition()
+      .duration(1000)
+      .attr('transform', 'translate(0, ' + (h - padding / 2) + ')')
+      .call(_this.xAxis)
+    d3.select('.y')
+      .transition()
+      .duration(1000)
+      .attr('transform', `translate(${padding / 2}, 0)`)
+      .call(_this.yAxis)
+  }
+
+  genInSigScatter(_this) {
+    let svg = _this.svg
+    let w = _this.w
+    let h = _this.h
+    //让边缘的点和坐标轴之间保留空隙
+    let padding = _this.padding
+    let pointSize = _this.pointSize
+    //初始化scale
+    _this.xScale = d3.scaleLinear()
+      .domain([d3.min(_this.inputSig, d => d[0]), d3.max(_this.inputSig, d => d[0])])
+      .range([padding, w - padding])
+    _this.yScale = d3.scaleLinear()
+      .domain([d3.min(_this.inputSig, d => d[1]), d3.max(_this.inputSig, d => d[1])])
+      .range([h - padding, padding])
+    //添加点
+    svg.selectAll('circle')
+      .data(_this.inputSig)
+      .enter()
+      .append('circle')
+      .attr('cx', d => _this.xScale(d[0]))
+      .attr('cy', d => _this.yScale(d[1]))
+      .attr('r', pointSize)
+      .attr("fill", "rgb(149, 180, 54)")
+    //初始化坐标轴
+    _this.xAxis = d3.axisBottom().scale(_this.xScale)
+    _this.yAxis = d3.axisLeft().scale(_this.yScale)
+    //添加坐标轴
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0, ' + (h - padding / 2) + ')')
+      .call(_this.xAxis)
+    svg.append('g')
+      .attr('class', 'y axis')
+      .attr('transform', `translate(${padding / 2}, 0)`)
+      .call(_this.yAxis)
+  }
+}
+
+Window.inSigScatter = new InSigScatter(chartStyle)
+
+function transInSigScatter(inSigArr) {
+  let w = 800; //x轴长
+  let h = 800; //y轴长
+  let padding = 100; //让边缘的点显示完全
+  let pointSize = 4;
+  let xScale = d3.scaleLinear()
+    .domain([d3.min(inSigArr, d => d[0]), d3.max(inSigArr, d => d[0])])
+    .range([padding, w - padding]);
+  let yScale = d3.scaleLinear()
+    .domain([d3.min(inSigArr, d => d[1]), d3.max(inSigArr, d => d[1])])
+    .range([h - padding, padding]);
+  let chart = d3.select('.input-signal')
+  chart.selectAll('circle')
+    .data(inSigArr)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d[0]))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', pointSize)
+    .attr("fill", "rgb(149, 180, 54)")
+
+  chart.selectAll('circle')
+    .data(inSigArr)
+    .exit()
+  // chart.selectAll("text")
+  //   .data(inSigArr)
+  //   .enter()
+  //   .append("text")
+  //   .text(function (d) {
+  //     return d[0] + "," + d[1];
+  //   })
+  //   .attr('transform', `translate(${pointSize + 2}, ${pointSize / 2})`)
+  //   .attr("x", function (d) {
+  //     return xScale(d[0]);
+  //   })
+  //   .attr("y", function (d) {
+  //     return yScale(d[1]);
+  //   })
+  //   .attr("font-family", "sans-serif")
+  //   .attr("font-size", "14px")
+  //   .attr("fill", "rgb(149, 180, 54)");
+  xAxis.scale(xScale);
+  chart.select('.xAxis')
+    .transition()
+    .duration(1000)
+    .call(xScale)
+  yAxis.scale(yScale);
+  chart.select('.yAxis')
+    .transition()
+    .duration(1000)
+    .call(yScale)
+
+}
+
+function genOutSig() {
+  if (!userInput) {
+    genInSig()
+  }
+  outSigBar(userInput)
+}
+
+function outSigBar(inSig) {
+  outSig = new Signal(sig(inSig))
+  FFTed = outSig.FFT().output //FFT变换后的信号
+  amplitude = FFTed.map(val => Math.sqrt(val.r ** 2 + val.i ** 2))
+  phase = FFTed.map(val => Math.atan(val.i / val.r))
+  real = FFTed.map(val => val.r)
+  imaginary = FFTed.map(val => val.i)
+
+  console.log(`${amplitude}\n${phase}\n${real}\n${imaginary}`);
+
+  let w = 500
+  let h = 500
+
+  bar(amplitude, '.amplitude')
+
+  function bar(dataset, el) {
+    let xScale = d3.scaleBand()
+      .domain(d3.range(dataset.length))
+      .rangeRound([0, w])
+      .paddingInner(.05)
+
+    let yScale = d3.scaleLinear()
+      .domain([0, d3.max(dataset)])
+      .range([0, h])
+
+    let chart = d3.select(el)
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h)
+
+    chart.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => xScale(i))
+      .attr('y', d => h - yScale(d))
+      .attr('width', xScale.bandwidth())
+      .attr('height', d => yScale(d))
+      .attr('fill', 'rgb(149, 180, 54)')
+
+    chart.selectAll("text")
+      .data(dataset)
+      .enter()
+      .append("text")
+      .text(function (d) {
+        return d;
+      })
+      .attr("text-anchor", "middle")
+      .attr("x", (d, i) => xScale(i) + xScale.bandwidth() / 2)
+      .attr("y", d => h - yScale(d) + 14)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "14px")
+      .attr("fill", "white");
+  }
+}
